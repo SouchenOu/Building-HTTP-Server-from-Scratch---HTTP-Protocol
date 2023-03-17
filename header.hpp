3f256ed3a015868dbe97e9f4613de36a85f417d3
Soukaina Ouchen <souchen@e1r6p1.1337.ma>
2023-03-17 09:40:23 +0100

    start

--- header.hpp
+++ header.hpp
@@ -1,19 +1,13 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   main.cpp                                           :+:      :+:    :+:   */
+/*   header.hpp                                         :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: souchen <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2023/03/10 09:36:29 by souchen           #+#    #+#             */
-/*   Updated: 2023/03/16 16:51:13 by souchen          ###   ########.fr       */
+/*   Created: 2023/03/16 16:51:19 by souchen           #+#    #+#             */
+/*   Updated: 2023/03/16 16:51:21 by souchen          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 
-/*working on the simplest component of a web server or the most basic component of programming in networks that is a sockets
-// and a socket is just a thing that allows your operating system to connect to a network in  a particular way, and 
-you would use this whether you are building a server a client or a peer-to-peer network
-
-
-*/
